05b147bf148a871d929034a85897a2b0002b3644
mhudyx <mhudyx@gmail.com>
2022-01-12 13:51:03 +0100

    Configure connection with mysql database

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class Car {
     private JPanel Main;
@@ -21,10 +22,28 @@ public class Car {
         frame.pack();
         frame.setVisible(true);
     }
+    Connection con;
+    PreparedStatement pst;
 
+    public void connect() {
+
+
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/car?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "zaq1@WSX");
+            System.out.println("Success!!!!!!!!!!!!!!!");
+
+        } catch (ClassNotFoundException ex) {
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public Car() {
+        connect();
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
